f5fb8db7439610c57c409580fd4f7be28fb9a98b
TomOS3 <82455174+TomOS3@users.noreply.github.com>
2021-09-09 14:41:21 +0200

    Add files via upload

---
+++ DetectRogueDHCP.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from scapy.all import *
+
+conf.checkIPaddr = False
+fam,hw = get_if_raw_hwaddr("tun0")
+
+print("conf.iface:"+str(conf.iface)) #wlp2s0, or tun0
+print("fam:"+str(fam))
+print("fam:"+str(hw))
+dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type","discover"),"end"])
+ans, unans = srp(dhcp_discover, multi=True, timeout=3)      # Press CTRL-C after several seconds --> timeout :)
+for p in ans: print(p[1][Ether].src, p[1][IP].src)
+
