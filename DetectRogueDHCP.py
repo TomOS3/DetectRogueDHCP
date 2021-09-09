703f161c831137d9125cd85c4103f8353f1f23ef
TomOS3 <82455174+TomOS3@users.noreply.github.com>
2021-09-09 15:41:38 +0200

    Update DetectRogueDHCP.py

--- DetectRogueDHCP.py
+++ DetectRogueDHCP.py
@@ -2,13 +2,16 @@
 
 from scapy.all import *
 
-conf.checkIPaddr = False
-fam,hw = get_if_raw_hwaddr("tun0")
+def check(interfaceName):
+  conf.checkIPaddr = False
+  fam,hw = get_if_raw_hwaddr(interfaceName)
+  dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type","discover"),"end"])
+  ans, unans = srp(dhcp_discover, multi=True, timeout=3)      # Press CTRL-C after several seconds --> timeout :)
+  for p in ans: print(p[1][Ether].src, p[1][IP].src)
 
-print("conf.iface:"+str(conf.iface)) #wlp2s0, or tun0
-print("fam:"+str(fam))
-print("fam:"+str(hw))
-dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type","discover"),"end"])
-ans, unans = srp(dhcp_discover, multi=True, timeout=3)      # Press CTRL-C after several seconds --> timeout :)
-for p in ans: print(p[1][Ether].src, p[1][IP].src)
 
+interfaces = ["ens160","ens192","ens224","ens256"]
+for i in interfaces:
+  print("===================")
+  print("Checking: "+i)
+  check(i)
